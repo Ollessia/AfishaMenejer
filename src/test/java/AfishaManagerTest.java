7a505edea3de8ed2c41f9383cc16d5e0f2a69a82
Olesia <lisa999lisa@mail.ru>
2024-01-22 20:44:34 +0300

    made a change

---
+++ src/test/java/AfishaManagerTest.java
@@ -0,0 +1,67 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AfishaManagerTest {
+
+    @Test
+    public void add0Film() {
+        AfishaManager manager = new AfishaManager();
+        String[] actual = manager.findAll();
+        String[] expected = {};
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void add1Film() {
+        AfishaManager manager = new AfishaManager();
+        manager.addFilm("Film1");
+        String[] actual = manager.findAll();
+        String[] expected = {"Film1"};
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void add4Film() {
+        AfishaManager manager = new AfishaManager();
+        manager.addFilm("Film1");
+        manager.addFilm("Film2");
+        manager.addFilm("Film3");
+        manager.addFilm("Film4");
+        String[] actual = manager.findAll();
+
+        String[] expected = {"Film1", "Film2", "Film3", "Film4"};
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+
+    @Test
+    public void findLast() {
+        AfishaManager manager = new AfishaManager();
+        manager.addFilm("Film1");
+        manager.addFilm("Film2");
+        manager.addFilm("Film3");
+        manager.addFilm("Film4");
+
+        String[] actual = manager.findLast();
+        String[] expected = {"Film4", "Film3", "Film2", "Film1"};
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void findAll() {
+        AfishaManager manager = new AfishaManager();
+        manager.addFilm("Film1");
+        manager.addFilm("Film2");
+        manager.addFilm("Film3");
+        manager.addFilm("Film4");
+        manager.addFilm("Film5");
+        String[] actual = manager.findAll();
+
+        String[] expected = {"Film1", "Film2", "Film3", "Film4", "Film5"};
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+
+}
